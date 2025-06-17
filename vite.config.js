9aff5d97df8a6713303e956729fde22f79760bd2
Andrii Butenko <andrejbutik@gmail.com>
2025-06-17 11:53:39 +0200

    Trying do photo

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
+    base: '/project-grosters/',
     build: {
       sourcemap: true,
       rollupOptions: {
