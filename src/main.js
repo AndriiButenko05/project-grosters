9df74cfe4eda6b8d8c0f74294cfa17c8cac6f074
Andrii Butenko <andrejbutik@gmail.com>
2025-06-17 15:59:40 +0200

    Finished mobile

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
-  document.addEventListener('DOMContentLoaded', function () {
+// HEADER
+const BurgerBtn = document.querySelector(".burger-btn");
+const mobileMenu = document.querySelector(".mobile-menu");
+const closeBtn = document.querySelector(".mobile-menu-icon-close");
+const navList = document.querySelector(".mobile-menu-navigation-list")
+BurgerBtn.addEventListener("click", () => {
+  document.body.classList.add('no-scroll');
+  mobileMenu.classList.add("is-open")
+})
+closeBtn.addEventListener("click", () => {
+  document.body.classList.remove('no-scroll');
+  mobileMenu.classList.remove("is-open")
+})
+navList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("mobile-menu-navigation-list-link")) {
+      document.body.classList.remove('no-scroll');
+    mobileMenu.classList.remove("is-open")
+    console.log(event.target);
+    
+    
+  }
+  console.log(event.target);
+  return;
+})
+// PLUSES
+document.addEventListener('DOMContentLoaded', function () {
     new Splide('#bulgaria-slider', {
       type: 'loop',
       autoplay: true,
-      interval: 5000,
+      interval: 50000,
       pauseOnHover: true,
       pagination: true,
       arrows: true,
@@ -10,18 +35,46 @@
     }).mount()
   });
 
+  // QUESTIONS
   const buttons = document.querySelectorAll('.accordion-button');
-
   buttons.forEach(button => {
     button.addEventListener('click', () => {
       const content = button.nextElementSibling;
 
-      // Закриваємо всі інші
       document.querySelectorAll('.accordion-content').forEach(el => {
         if (el !== content) el.style.maxHeight = null;
       });
 
-      // Перемикаємо поточну
       content.style.maxHeight = content.style.maxHeight ? null : content.scrollHeight + 'px';
     });
   });
+
+  // ROOT
+  document.addEventListener("DOMContentLoaded", function () {
+    const items = document.querySelectorAll(".advantages-item");
+    const section = document.querySelector(".root-section");
+  
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            items.forEach((item, index) => {
+              item.style.opacity = "0";
+              item.style.transform = "translateY(20px)";
+              item.style.transition = "none";
+  
+              setTimeout(() => {
+                item.style.transition = "opacity 1s ease, transform 1s ease";
+                item.style.opacity = "1";
+                item.style.transform = "translateY(0)";
+              }, index * 700); 
+            });
+          }
+        });
+      },
+      { threshold: 0.3 } 
+    );
+  
+    observer.observe(section);
+  });
+
