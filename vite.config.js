46190f8f53374e0e091ef4a6d47c6167badedc5d
Andrii Butenko <andrejbutik@gmail.com>
2025-07-17 18:07:25 +0200

    Doing pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/project-grosters/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
